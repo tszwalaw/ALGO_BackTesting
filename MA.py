9500172e0de08e35a4bda5c30651f6637217015f
Ocean <lawtszwa@gmail.com>
2020-12-29 15:54:17 +0000

    Separate the normal MA and the MA with stock info

--- MA.py
+++ MA.py
@@ -2,55 +2,66 @@ from StockInfo import StockInfo
 from StockPerDateData import StockPerDateData
 import math
 
-# Simple moving average (SMA)
-def CalSMA(stock_info, SMA_days):
+# Simple moving average (SMA) - stock info
+def CalSMA_StockInfo(stock_info, SMA_days):
+
+    value_list = []
     result = []
     
     data_map = stock_info.GetStockDayMapData()
-    avg_price = 0
+    ref_price = 0
     
     for x in range(len(data_map)):
-        if(x+1-SMA_days <= 0):
-            avg_price += data_map[x][1].GetClose()
-            if(x+1-SMA_days == 0):
-                pair = (data_map[x][0], avg_price/SMA_days)
-                result.append(pair)
-        else:
-            avg_price -= data_map[x-SMA_days][1].GetClose()
-            avg_price += data_map[x][1].GetClose()
+        ref_price = data_map[x][1].GetClose()
+        pair = (data_map[x][0], ref_price)
+        value_list.append(pair)
             
-            pair = (data_map[x][0], avg_price/SMA_days)
-            result.append(pair)
-            
+    result = CalSMA_ValueList(value_list, SMA_days)
     return result
-            
-# Exponential moving average (EMA)
-def CalEMA(stock_info, EMA_days):
+    
+# Exponential moving average (EMA) - stock info
+def CalEMA_StockInfo(stock_info, EMA_days):
 
-    """ Smoothing value """
-    smothing_constant = 2
-    smoothing = smothing_constant/(1+EMA_days)
-
+    value_list = []
+    result = []
+    data_map = stock_info.GetStockDayMapData()
+    ref_price = 0
+    
+    for x in range(len(data_map)):
+      ref_price = data_map[x][1].GetClose()
+      pair = (data_map[x][0], ref_price)
+      value_list.append(pair)
+        
+    result = CalEMA_ValueList(value_list, EMA_days)
+    return result
+    
+# Simple moving average with Volatility Changes - stock info
+def CalSMA_Vol(stock_info, SMA_days):
     result = []
     data_map = stock_info.GetStockDayMapData()
     avg_price = 0
-    last_ema = 0
+    sum_of_vol = 0
     
     for x in range(len(data_map)):
-        if(x+1-EMA_days <= 0):
-            avg_price += data_map[x][1].GetClose()
-            if(x+1-EMA_days == 0):
-                last_ema = avg_price/EMA_days
-                pair = (data_map[x][0], last_ema)
+        if x+1-SMA_days < 0:
+            avg_price += (data_map[x][1].GetHigh() + data_map[x][1].GetLow())/2 * data_map[x][1].GetVol()
+            sum_of_vol += data_map[x][1].GetVol()
+            if x+1-SMA_days == 0:
+                pair = (data_map[x][0], avg_price/sum_of_vol)
                 result.append(pair)
         else:
-            last_ema = (data_map[x][1].GetClose()) * smoothing + (last_ema * (1 - smoothing))
-            pair = (data_map[x][0], last_ema)
+            sum_of_vol -= data_map[x-SMA_days][1].GetVol()
+            sum_of_vol += data_map[x][1].GetVol()
+        
+            avg_price -= (data_map[x-SMA_days][1].GetHigh() + data_map[x-SMA_days][1].GetLow())/2 * data_map[x-SMA_days][1].GetVol()
+            avg_price += (data_map[x][1].GetHigh() + data_map[x][1].GetLow())/2 * data_map[x][1].GetVol()
+            
+            pair = (data_map[x][0], avg_price/sum_of_vol)
             result.append(pair)
 
     return result
-    
-#Exponential moving average with Volatility Changes
+
+#Exponential moving average with Volatility Changes - stock info
 def CalEMA_Vol(stock_info, EMA_days):
 
     """ Smoothing value """
@@ -65,10 +76,10 @@ def CalEMA_Vol(stock_info, EMA_days):
     current_value = 0
     
     for x in range(len(data_map)):
-        if(x+1-EMA_days <= 0):
+        if x+1-EMA_days <= 0:
             avg_price += (data_map[x][1].GetHigh() + data_map[x][1].GetLow())/2
             sum_of_vol += data_map[x][1].GetVol()
-            if(x+1-EMA_days == 0):
+            if x+1-EMA_days == 0:
                 last_ema = avg_price/EMA_days
                 pair = (data_map[x][0], last_ema)
                 result.append(pair)
@@ -85,3 +96,50 @@ def CalEMA_Vol(stock_info, EMA_days):
             result.append(pair)
 
     return result
+    
+# Simple moving average
+def CalSMA_ValueList(value_list, SMA_days):
+    result = []
+    
+    avg_price = 0
+    
+    for x in range(len(value_list)):
+        if x+1-SMA_days <= 0:
+            avg_price += value_list[x][1]
+            if(x+1-SMA_days == 0):
+                pair = (value_list[x][0], avg_price/SMA_days)
+                result.append(pair)
+        else:
+            avg_price -= value_list[x-SMA_days][1]
+            avg_price += value_list[x][1]
+            
+            pair = (value_list[x][0], avg_price/SMA_days)
+            result.append(pair)
+            
+    return result
+    
+# Exponential moving average (EMA)
+def CalEMA_ValueList(value_list, EMA_days):
+    """ Smoothing value """
+    smothing_constant = 2
+    smoothing = smothing_constant/(1+EMA_days)
+
+    result = []
+    avg_price = 0
+    last_ema = 0
+    
+    for x in range(len(value_list)):
+        if x+1-EMA_days <= 0:
+            avg_price += value_list[x][1]
+            if x+1-EMA_days == 0:
+                last_ema = avg_price/EMA_days
+                pair = (value_list[x][0], last_ema)
+                result.append(pair)
+        else:
+            last_ema = value_list[x][1] * smoothing + last_ema * (1 - smoothing)
+            pair = (value_list[x][0], last_ema)
+            result.append(pair)
+    
+    return result
+    
+
