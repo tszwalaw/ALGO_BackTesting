a12d8507ea864fdbde7a4cedba984cb292e88415
Ocean <lawtszwa@gmail.com>
2021-01-02 00:21:23 +0000

    Updated Line Chart

--- AlgoBackTesting.py
+++ AlgoBackTesting.py
@@ -5,39 +5,72 @@ from MACD import *
 from RSI import *
 from LineChart import *
 
-stock_name = "INTC"
-checking_days = 3650
-
-stock = StockInfo(stock_name, checking_days)
-stock.StartToLoadData()
+# Chart(s)
+line_chart = LineChart()
 
 """
-Indicator
+INTC wth MACD & RSI
 """
+INTC = StockInfo("INTC", 365)
+INTC.StartToLoadData()
 
-#MACD
-MACD = CalMACD(stock, 12, 26, CONST_MACD_EMA)
+line_chart.NewChart(INTC.GetStockName(), ["MACD", "RSI"], 2)
+
+# Main Stock Info Chart with MA(s)
+line_chart.AddToMainChart(INTC.GetStockInfo(CONST_STOCK_CLOSE), "INTC")
+MA_10 = CalSMA_StockInfo(INTC, 10)
+MA_20 = CalSMA_StockInfo(INTC, 20)
+MA_50 = CalSMA_StockInfo(INTC, 50)
+line_chart.AddToMainChart(MA_10, "MA 10")
+line_chart.AddToMainChart(MA_20, "MA 20")
+line_chart.AddToMainChart(MA_50, "MA 50")
+
+# MACD
+MACD = CalMACD(INTC, 12, 26, CONST_MACD_EMA)
 signal_line = CalSignalLine(MACD, 9, CONST_MACD_EMA)
-signal_line_vs_MACD = CalSignalLineVsMACD(stock, 12, 26, 9, CONST_MACD_EMA)
+signal_line_vs_MACD = CalSignalLineVsMACD(INTC, 12, 26, 9, CONST_MACD_EMA)
+line_chart.AddToSubChart(MACD, 0, "MACD")
+line_chart.AddToSubChart(signal_line, 0, "Signal Line")
+line_chart.AddToSubChart(signal_line_vs_MACD, 0, "Signal Line vs MACD")
 
-MACD_chart = LineChart(stock.GetStockName(), "MACD")
-MACD_chart.AddMainData(stock.GetStockInfo(CONST_STOCK_CLOSE))
-MACD_chart.AddSubData(MACD)
-MACD_chart.AddSubData(signal_line)
-MACD_chart.AddSubData(signal_line_vs_MACD)
-MACD_chart.PlotChart()
+# RSI
+RSI = CalRSI(INTC, 14, CONST_RSI_SMA)
+line_chart.AddToSubChart(RSI, 1, "RSI")
+line_chart.AddHorizontalLineToSubChart(30, 1)
+line_chart.AddHorizontalLineToSubChart(70, 1)
 
-
-#RSI
-RSI = CalRSI(stock, 14, CONST_RSI_SMA)
-RSI_chart = LineChart(stock.GetStockName(), "RSI")
-RSI_chart.AddMainData(stock.GetStockInfo(CONST_STOCK_CLOSE))
-RSI_chart.AddSubData(RSI)
-RSI_chart.PlotChart()
+line_chart.PlotChart()
 
 """
-ALGO
+NFLX
 """
+NFLX = StockInfo("NFLX", 365)
+NFLX.StartToLoadData()
+
+line_chart.NewChart(NFLX.GetStockName(), ["RSI-14 vs RSI-28 (SMA)", "RSI-14 vs RSI-28 (EMA)"], 2)
+line_chart.AddToMainChart(NFLX.GetStockInfo(CONST_STOCK_HIGH), "HIGH")
+line_chart.AddToMainChart(NFLX.GetStockInfo(CONST_STOCK_LOW), "LOW")
+
+# RSI-14 vs RSI-28 in SMA
+RSI_SMA_14 = CalRSI(INTC, 14, CONST_RSI_SMA)
+RSI_SMA_28 = CalRSI(INTC, 28, CONST_RSI_SMA)
+line_chart.AddToSubChart(RSI_SMA_14, 0, "RSI SMA 14")
+line_chart.AddToSubChart(RSI_SMA_28, 0, "RSI SMA 28")
+line_chart.AddHorizontalLineToSubChart(30, 0)
+line_chart.AddHorizontalLineToSubChart(70, 0)
+
+# RSI-14 vs RSI-28 in EMA
+RSI_EMA_14 = CalRSI(INTC, 14, CONST_RSI_EMA)
+RSI_EMA_28 = CalRSI(INTC, 28, CONST_RSI_EMA)
+line_chart.AddToSubChart(RSI_EMA_14, 1, "RSA EMA 14")
+line_chart.AddToSubChart(RSI_EMA_28, 1, "RSA EMA 28")
+line_chart.AddHorizontalLineToSubChart(30, 1)
+line_chart.AddHorizontalLineToSubChart(70, 1)
+
+line_chart.PlotChart()
+
+# Show Chart(s)
+line_chart.ShowChart()
 
 
 
--- LineChart.py
+++ LineChart.py
@@ -4,44 +4,89 @@ from datetime import date
 
 
 class LineChart:
-
-    main_data = []
-    sub_data = []
-    stock_name = ""
-    indicator_name = ""
     
-    def __init__(self, stock_name, indicator_name):
+    def __init__(self):
+        self.stock_name = []
+        # Main Chart
         self.main_data = []
+        self.main_chart_line_name = []
+        # Sub Chart
+        self.indicator_name = []
         self.sub_data = []
+        self.horizontal_line = []
+        self.sub_chart_line_name = []
+        self.sub_chart_total = 0
+        
+    def NewChart(self, stock_name, indicator_name, sub_chart_total):
         self.stock_name = stock_name
         self.indicator_name = indicator_name
+        self.sub_chart_total = sub_chart_total
         
-    def AddMainData(self, data):
-        self.main_data.append(data)
+        # Main Chart Reset
+        self.main_data = []
+        self.main_chart_line_name = []
+        # Sub Chart Reset
+        self.sub_data = []
+        self.horizontal_line = []
+        self.sub_chart_line_name = []
 
-    def AddSubData(self, data):
-        self.sub_data.append(data)
+        for x in range(sub_chart_total):
+            self.sub_data.append([])
+            self.horizontal_line.append([])
+            self.sub_chart_line_name.append([])
+        
+    def AddToMainChart(self, data, name):
+        self.main_data.append(data)
+        self.main_chart_line_name.append(name)
+
+    def AddToSubChart(self, data, sub_chart_num, sub_chart_name):
+        if sub_chart_num < 0 or sub_chart_num >= self.sub_chart_total:
+            return
+        self.sub_data[sub_chart_num].append(data)
+        self.sub_chart_line_name[sub_chart_num].append(sub_chart_name)
+        
+    def AddHorizontalLineToSubChart(self, value, sub_chart_num):
+        if sub_chart_num < 0 or sub_chart_num >= self.sub_chart_total:
+            return
+        self.horizontal_line[sub_chart_num].append(value)
         
     def PlotChart(self):
+    
+        
         self.AdjData()
-        fig, chart = plt.subplots(2, 1)
+        fig, chart = plt.subplots(self.sub_chart_total+1, 1)
+        fig.canvas.set_window_title(self.stock_name)
+        
         chart[0].set_title(self.stock_name)
-        chart[0].set_xlabel("Price")
-        chart[0].set_ylabel("Date")
+        chart[0].set_xlabel("Date")
+        chart[0].set_ylabel("Price")
         
         for k in range(len(self.main_data)):
             x,y = zip(*self.main_data[k])
-            chart[0].plot(x,y)
+            chart[0].plot(x, y, label = self.main_chart_line_name[k])
+
+        chart[0].legend()
             
-        chart[1].set_title(self.indicator_name)
-        chart[1].set_xlabel("Value")
-        chart[1].set_ylabel("Date")
+            
+        for t in range(self.sub_chart_total):
+            chart[t+1].set_title(self.indicator_name[t])
+            chart[t+1].set_xlabel("Date")
+            chart[t+1].set_ylabel("Value")
+            
+            for k in range(len(self.sub_data[t])):
+                x,y = zip(*self.sub_data[t][k])
+                chart[t+1].plot(x, y, label = self.sub_chart_line_name[t][k])
+                
+            chart[t+1].legend()
+                
+            if len(self.horizontal_line[t]) > 0:
+                for x in self.horizontal_line[t]:
+                    chart[t+1].axhline(y=x, color='r', linestyle='-.')
         
-        for k in range(len(self.sub_data)):
-            x,y = zip(*self.sub_data[k])
-            chart[1].plot(x,y)
-            
         fig.autofmt_xdate()
+        
+    def ShowChart(self):
+
         plt.show()
         
     def AdjData(self):
@@ -50,8 +95,9 @@ class LineChart:
             if self.main_data[x][0][0] > start_date:
                 start_date = self.main_data[x][0][0]
         for x in range(len(self.sub_data)):
-            if self.sub_data[x][0][0] > start_date:
-                start_date = self.sub_data[x][0][0]
+            for y in range(len(self.sub_data[x])):
+                if self.sub_data[x][y][0][0] > start_date:
+                    start_date = self.sub_data[x][y][0][0]
                 
         for x in range(len(self.main_data)):
             while True:
@@ -63,13 +109,14 @@ class LineChart:
                     break
         
         for x in range(len(self.sub_data)):
-            while True:
-                if self.sub_data[x][0][0] != start_date:
-                    self.sub_data[x].pop(0)
-                else:
-                    break
-                if len(self.sub_data[x]) == 0:
-                    break
+            for y in range(len(self.sub_data[x])):
+                while True:
+                    if self.sub_data[x][y][0][0] != start_date:
+                        self.sub_data[x][y].pop(0)
+                    else:
+                        break
+                    if len(self.sub_data[x][y]) == 0:
+                        break
                 
             
         
